d0764a9fe8de3e4464e9a836bdf9d70b4de58524
AnandhuKrisB <anandhukrishna10b@gmail.com>
2025-08-05 00:37:13 +0530

    Second commit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import numpy as np
 
-arr = np.array([1,2,3,4])
+arr = np.array([[['A', 'B', 'C'], ['D', 'E', 'F']], 
+                [['H', 'I', 'J'], ['K', 'L', 'L']],
+                [['M', 'N', 'O'], ['P', 'Q', 'R']]])
 
-arr = arr * 2
 
-print(arr)
+print(arr[2, 1, 0])
