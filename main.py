07ecf70ae24626c2b927ab0dda918af5adbfa621
AnandhuKrisB <anandhukrishna10b@gmail.com>
2025-08-06 00:07:45 +0530

    Slicing, Arithmetic and broadcasting completed

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 import numpy as np
 
-arr = np.array([[['A', 'B', 'C'], ['D', 'E', 'F']], 
-                [['H', 'I', 'J'], ['K', 'L', 'L']],
-                [['M', 'N', 'O'], ['P', 'Q', 'R']]])
+arr = np.array([1, 2, 3, 4])
 
+# SLICING
+# print(arr[0:4:2])
+# print(arr[:, 0])
+# print(arr[:, 1:2])
+# print(arr[1:4, 1:4])
+# print(arr[1:4, 1:4])
 
-print(arr[2, 1, 0])
+# ARITHMETIC 
+
+# print(arr - 1)
+# print(np.max(arr))
+# print(np.pi * arr**2)
+
+# arr[arr >= 4] = 0
+# print(arr)
+
+# BROADCASTING
+
+a1 = np.array([[1, 2, 3, 4]])
+a2 = np.array([[1], [2], [3], [4]])
+
+print(a1 * a2)
+
