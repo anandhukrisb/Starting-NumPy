f6545ee1bd7527e4fb49b9a9dd27196d435aa559
AnandhuKrisB <anandhukrishna10b@gmail.com>
2025-09-09 22:34:53 +0530

    revising

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import numpy as np
 
-arr = np.array([1, 2, 3, 4])
+arr = np.array([[[1, 2, 3, 4], [5, 6, 7, 8 ], [9, 10, 11, 12]],
+                [[13, 14, 15, 16], [17, 18, 19, 20], [21, 22, 23, 24]],
+                [[25, 26, 27, 28], [29, 30, 31, 32], [33, 34, 35, 36]]])  
 
 # SLICING
-# print(arr[0:4:2])
+# print(arr[0:3:2])
 # print(arr[:, 0])
 # print(arr[:, 1:2])
 # print(arr[1:4, 1:4])
@@ -23,8 +25,8 @@ arr = np.array([1, 2, 3, 4])
 a1 = np.array([[1, 2, 3, 4]])
 a2 = np.array([[1], [2], [3], [4]])
 
-print(a1.shape)
-print(a2.shape)
+# print(a1.shape)
+# print(a2.shape)
 
 # print(a1 * a2)
 
